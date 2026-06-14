44bc867e517b28d76637e2ccdb1489f55cff567c
Hong seonghui <dream42us@gmail.com>
2026-06-14 18:32:05 +0900

    Include guide.html in build output

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/markdown-editor/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        guide: resolve(__dirname, 'guide.html')
+      }
+    }
+  }
 });
